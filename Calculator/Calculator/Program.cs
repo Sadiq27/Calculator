c1c8ca6e17ae7470e43b5ca8365ae12c4707c4a6
Sadiq <samedov.sadiq55@gmail.com>
2023-05-26 12:17:57 +0400

    Add Menu

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("===== Calculator =====");
+                Console.WriteLine("1. Addition ( + )");
+                Console.WriteLine("2. Subtraction ( - )");
+                Console.WriteLine("3. Multiplication ( * )");
+                Console.WriteLine("4. Division ( / )");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("--------------------");
+                Console.Write("Select an operation: ");
+
+                int input = Convert.ToInt32(Console.ReadLine());
+
+            }
         }
     }
 }
